7c8ee82f0bce20196935446dd3db47c16af65861
朱建刚 <wb-zhujiangang@taobao.com>
2013-02-26 09:49:30 +0800

    changed demo and button title

--- LeveyPopListViewDemo/AppDelegate.m
+++ LeveyPopListViewDemo/AppDelegate.m
@@ -19,7 +19,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     UIButton *demoBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [demoBtn setTitle:@"亚美得" forState:UIControlStateNormal];
+    [demoBtn setTitle:@"Show List" forState:UIControlStateNormal];
     demoBtn.frame = CGRectMake(100, 220, 120, 44);
     [demoBtn addTarget:self action:@selector(showListView) forControlEvents:UIControlEventTouchUpInside];
     [self.window addSubview:demoBtn];
@@ -27,8 +27,7 @@
     _infoLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, 320, 30)];
     _infoLabel.textAlignment = UITextAlignmentCenter;
     [self.window addSubview:_infoLabel];
-    /*
-    _options = [[NSArray arrayWithObjects:
+    _options = [NSArray arrayWithObjects:
                  [NSDictionary dictionaryWithObjectsAndKeys:[UIImage imageNamed:@"facebook.png"],@"img",@"Facebook",@"text", nil],
                  [NSDictionary dictionaryWithObjectsAndKeys:[UIImage imageNamed:@"twitter.png"],@"img",@"Twitter",@"text", nil], 
                  [NSDictionary dictionaryWithObjectsAndKeys:[UIImage imageNamed:@"tumblr.png"],@"img",@"Tumblr",@"text", nil], 
@@ -37,13 +36,8 @@
                  [NSDictionary dictionaryWithObjectsAndKeys:[UIImage imageNamed:@"pinterest.png"],@"img",@"Pinterest",@"text", nil], 
                  [NSDictionary dictionaryWithObjectsAndKeys:[UIImage imageNamed:@"dribbble.png"],@"img",@"Dribbble",@"text", nil], 
                  [NSDictionary dictionaryWithObjectsAndKeys:[UIImage imageNamed:@"deviant-art.png"],@"img",@"deviantArt",@"text", nil], 
-                 nil] retain];
-     */
-    
-    _options = @[@{@"text" : @"Twitter", @"img" : [UIImage imageNamed:@"twitter.png"]},
-                 @"Facebook2", @"Facebook3",
-  @{@"text" : @"LinkedIn", @"img" : [UIImage imageNamed:@"dribbble.png"]}];
-    
+                 nil];
+        
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
